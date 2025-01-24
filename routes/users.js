82bb126894aeb6e3abbf24ea994aad7e69416611
Your name <Your email>
2025-01-24 15:57:53 +0100

    url front backend setup

--- routes/users.js
+++ routes/users.js
@@ -3,13 +3,15 @@ var router = express.Router();
 const dd =new Date();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
 router.get('/year', (req, res)=> {
   const Date = "year: "+ (dd.getFullYear());
   res.json({Date})
 })
+
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 
 module.exports = router;
